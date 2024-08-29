2aa08a0a365206bdc5247b51ccd7059f866ba593
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-29 12:30:40 -0300

    favicon uploaded & style changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
+import Header from './components/Header';
+
 function App() {
   return (
     <>
-      <h1>Note Code</h1>
+      <div className="bg-bg-image absolute top-0 -z-10 h-[70vh] w-full bg-cover bg-center bg-no-repeat" />
+
+      <div className="container mx-auto px-4 py-6">
+        <Header />
+        {/* main */}
+      </div>
     </>
   );
 }
