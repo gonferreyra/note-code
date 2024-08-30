52e1291eca235fddeb070b22e815887b8ed3642e
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-08-30 10:40:50 -0300

    routes implemented

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CodeComponent from './components/CodeComponent.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <CodeComponent />,
+      },
+      {
+        path: '/:id',
+        element: <CodeComponent />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
