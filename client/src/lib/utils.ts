efb170d649dfe2d32d9c5415f747ff330eaab5d6
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-05 16:23:09 -0300

    fetch function moved to utils file

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,3 +1,24 @@
+import type { Code } from './types';
+
 export function sleep(time: number) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+const apiUrl = import.meta.env.VITE_BASE_API_URL;
+
+export const fetchSharedCode = async (id: string) => {
+  await sleep(2000);
+  const response = await fetch(`${apiUrl}/users?id=${id}`);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
+
+  const data = await response.json();
+
+  return {
+    code: data.map((code: Code) => code.code),
+    language: data.map((code: Code) => code.language),
+  };
+};
