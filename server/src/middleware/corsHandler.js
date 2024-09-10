c1f65452224dedf1c94d5da8a47e90d13c72bf2e
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-10 12:24:50 -0300

    cors middleware edited

--- server/src/middleware/corsHandler.js
+++ server/src/middleware/corsHandler.js
@@ -1,10 +1,10 @@
 export function corsHandler(req, res, next) {
   res.header('Access-Control-Allow-Origin', req.header('origin'));
-  res.header(
-    'Access-Control-Allow-Credentials',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Origin, X-Requested-With, Accept, Authorization',
+  );
 
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
